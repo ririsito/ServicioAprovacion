3910be15a801860c0465be7546e200ae14b46f2d
abigailmagallan <abigailmagallan@users.noreply.github.com>
2019-08-06 16:53:31 -0500

    Comit nuevo servicio.
    Servicio  Commit .

--- ServicioWebAutorizacionPermisos/ServicioWebAutorizacionPermisos/Controllers/PermisosController.cs
+++ ServicioWebAutorizacionPermisos/ServicioWebAutorizacionPermisos/Controllers/PermisosController.cs
@@ -34,8 +34,36 @@ namespace ServicioWebAutorizacionPermisos.Controllers
             Permisos Permiso = BD.Permisos.Single(x => x.Id == IdPer);
             Permiso.IdEstado = IdEstado;
             BD.SaveChanges();
+            updateRangoFechas(Permiso, IdEstado);
         }
-      
+        //update vacaciones
+        public void updateRangoFechas(Permisos permiso, int IdEstado)
+        {
+            
+            var desc = permiso.Descripcion;
+            var numero = permiso.Numero;
+            var fechaFinal = permiso.FechaRegreso;
+            var tipoPermiso = permiso.TipoPermiso;
+            var horaSalida = permiso.HoraSalida;
+            var listado = BD.Permisos.ToList();
+
+            if (tipoPermiso.Equals("Vacaciones") || tipoPermiso.Equals("Salida y Regreso")|| 
+                tipoPermiso.Equals("Incapacidad")||tipoPermiso.Equals("Sin Asistencia"))
+            {
+
+                var listadoRango = listado.Where(x => x.Numero == numero && x.Descripcion == desc 
+                && x.FechaRegreso == fechaFinal && x.TipoPermiso ==tipoPermiso && x.HoraSalida== horaSalida).ToList();
+                for (int i = 0; i < listadoRango.Count; i++)
+                {                    
+                    var Idpermiso = listadoRango[i].Id;
+                    Permisos Permiso = BD.Permisos.Single(x => x.Id == Idpermiso);
+                    Permiso.IdEstado = IdEstado;
+                    BD.SaveChanges();
+                }
+
+            }
+        }
+
 
     }
 }
